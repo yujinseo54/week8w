e458b230a8c465020661fd510d3f37a1a55edb53
yujinseo54 <71596123+yujinseo54@users.noreply.github.com>
2020-11-02 03:37:28 +0900

    upload files

---
+++ js/script.js
@@ -0,0 +1,45 @@
+$(document).ready(function(){
+
+	$(".poster").click(function(){
+		
+        $(".t1").css({
+        'display':'inline-block'
+        })
+	 })
+
+	$(".t1").click(function(){
+        $(".t2").css({
+        'display':'inline-block'
+            })
+	 })
+
+	$(".t2").click(function(){
+        $(".t3").css({
+        'display':'inline-block'
+            })
+	 })
+
+	$(".t3").click(function(){
+        $(".t4").css({
+        'display':'inline-block'
+            })
+	 })
+
+	$(".t4").click(function(){
+
+        $(".t5").css({
+        'display':'inline-block'
+            })
+	 })
+
+	$(".t5").click(function(){
+
+        $(".d1").css({
+        'display':'inline-block'
+        })
+	 })
+
+})
+
+        
+
